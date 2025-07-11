8fb993c29cbd79af01fb28972aabc42ddaf48ead
ronicairo <roni.cairo@outlook.fr>
2025-07-11 14:05:10 +0200

    style + menu

--- MySite/JobRecord/views.py
+++ MySite/JobRecord/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from JobRecord import views
+from FeedBack import views
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'feedback_home.html')
